a40d87708e29fc4a3d7ec6bc2866ec913094031c
Tanu soni <Tanusoni2312@gmail.com>
2023-08-18 23:29:01 +0530

    Okay

--- src/main/java/com/email/controller/EmailController.java
+++ src/main/java/com/email/controller/EmailController.java
@@ -1,14 +1,19 @@
 package com.email.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.email.entities.EmailDetails;
+import com.email.entities.EmailResponse;
 import com.email.service.EmailService;
 
 @RestController
+@CrossOrigin
 public class EmailController {
 	@Autowired
 	private EmailService emailService;
@@ -19,14 +24,15 @@ public class EmailController {
 	}
 
 	@PostMapping("/sendmail")
-	public String sendMail(@RequestBody EmailDetails details) {
+	public ResponseEntity<?> sendMail(@RequestBody EmailDetails details) {
 		boolean f = emailService.sendMail(details);
 		System.out.println(f);
 		System.out.println(details);
 		if (f)
-			return "Email Send Successfully";
+			return ResponseEntity.ok(new EmailResponse("Email Sent Successfully!!"));
 		else
-			return "Something Went Wrong";
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new EmailResponse("Email Not Sent!!"));
+				
 
 	}
 }
